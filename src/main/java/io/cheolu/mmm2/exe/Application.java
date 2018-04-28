30484756149c5283e8559b8066aae50c91198bc6
Administrator <Administrator@192.168.55.157>
2018-04-28 20:51:20 +0900

    cs

--- src/main/java/io/cheolu/mmm2/exe/Application.java
+++ src/main/java/io/cheolu/mmm2/exe/Application.java
@@ -2,11 +2,10 @@ package io.cheolu.mmm2.exe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("io.cheolu.mmm2")
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
